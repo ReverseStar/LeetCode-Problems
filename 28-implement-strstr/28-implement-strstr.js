eefea7e6f4f327b7b3c3d2fddbce1ea75bfe7336
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-08-08 18:59:05 +0600

    Time: 68 ms (85.65%), Space: 42.1 MB (51.75%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -4,16 +4,7 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    let cnt = 0;
-    let arr = haystack.split('');
-    let arr2 = needle.split('');
-    //console.log(arr,arr2);
-    
+  
     return haystack.search(needle);
 
-    if(needle === ''){
-        return 0;
-    } else{
-        return -1;
-    }
 };
