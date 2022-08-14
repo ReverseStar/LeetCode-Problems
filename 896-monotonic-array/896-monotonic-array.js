e8577620ce6dee98bc0d67cc79a7c64b277cf788
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-08-15 00:35:30 +0600

    Time: 773 ms (5.21%), Space: 84 MB (5.05%) - LeetHub

--- 896-monotonic-array/896-monotonic-array.js
+++ 896-monotonic-array/896-monotonic-array.js
@@ -4,8 +4,8 @@
  */
 var isMonotonic = function(nums) {
     let arr = nums.join('');
-    let asc = nums.slice().sort((a,b) => a - b).join('');
-    let desc = nums.slice().sort((a,b) => b - a).join('');
+    let asc = nums.sort((a,b) => a - b).join('');
+    let desc = nums.sort((a,b) => b - a).join('');
     // console.log(nums,asc, desc)
     
     if((arr === asc) || (arr === desc)){
