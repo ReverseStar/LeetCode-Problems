985374a88271cffc1067b598bd5553deac47a4cb
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-08-28 19:44:38 +0600

    Time: 92 ms (40.33%), Space: 42.2 MB (27.90%) - LeetHub

--- 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.js
+++ 2278-percentage-of-letter-in-string/2278-percentage-of-letter-in-string.js
@@ -5,13 +5,10 @@
  */
 var percentageLetter = function(s, letter) {
     let freq = _.countBy(s);
-    //console.log(freq);
     let cnt = freq[letter];
-    //console.log(cnt);
+    
     if(!(s.split('').includes(letter))){
         return 0;
     }
-    
-    let par = Math.floor((cnt/s.length)*100);
-    return par;
+    return Math.floor((cnt/s.length)*100);
 };
