e4b86a38d3f7424563d0c1bb7e6d7f686c3c965e
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-09-03 23:40:21 +0600

    Time: 139 ms (5.01%), Space: 42.7 MB (14.59%) - LeetHub

---
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} index
+ * @return {number[]}
+ */
+var createTargetArray = function(nums, index) {
+    let arr = [];
+    
+    for(let i=0;i<nums.length;i++){
+        arr.splice(index[i], 0, nums[i])
+    }
+    console.log(arr);
+    return arr;
+};
