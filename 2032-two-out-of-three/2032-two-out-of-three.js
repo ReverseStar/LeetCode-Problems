8260deea4781bf91c76c5983170800947c1876a1
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-09-18 21:28:03 +0600

    Time: 208 ms (5.11%), Space: 50.5 MB (5.11%) - LeetHub

--- 2032-two-out-of-three/2032-two-out-of-three.js
+++ 2032-two-out-of-three/2032-two-out-of-three.js
@@ -8,24 +8,16 @@ var twoOutOfThree = function(nums1, nums2, nums3) {
     let num1 = [...new Set(nums1)];
     let num2 = [...new Set(nums2)];
     let num3 = [...new Set(nums3)];
-    // console.log(num1, num2, num3);
-    let mx = Math.max(num1.length, num2.length, num3.length);
-    let cnt = 0;
+    console.log(num1,num2,num3);
     let arr = [];
-    
     let aaa = [...num1, ...num2, ...num3];
     let aa = _.countBy(aaa);
     
-    // console.log(aa,typeof(aa));
-    
     let keys = Object.keys(aa);
-    // console.log(keys);
-    
     keys.forEach(key => {
         if(aa[key] >= 2){
             arr.push(key)
         }
     })
-    // console.log(arr);
     return arr;
 };
