afc28e5cbcce8ec61e59937db34ff7571fe21cfc
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-11-14 20:28:23 +0600

    Time: 98 ms (100.00%), Space: 42.7 MB (100.00%) - LeetHub

---
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} celsius
+ * @return {number[]}
+ */
+var convertTemperature = function(celsius) {
+    let ans = [];
+    
+    ans.push(celsius + 273.15);
+    ans.push(celsius * 1.80 + 32.00);
+    
+    console.log(ans);
+    return ans;
+};
