56da944cc11f6449d2ccc59e4f8b056dfc66c020
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2022-11-23 18:38:27 +0600

    Time: 133 ms (26.33%), Space: 44.2 MB (38.67%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let arr = parseInt(x.toString().split('').reverse().join(''));
+    if(arr > 2**31){
+        return 0;
+    }
+    // else if(x < 0){
+    //      return BigInt(arr) * -1n;
+    // }else{
+    //     return BigInt(arr);
+    // }
+    return x < 0 ? BigInt(arr) * -1n : BigInt(arr);
+};
