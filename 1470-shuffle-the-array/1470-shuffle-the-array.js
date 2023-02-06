fc6fe4a50e301a71b62cbaf7b752e88bfe370a27
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-02-06 13:36:44 +0600

    Time: 63 ms (97.10%), Space: 45 MB (10.84%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -5,16 +5,13 @@
  */
 var shuffle = function(nums, n) {
     let arr = [];
-    let half = nums.length / 2;
     
-    let first = nums.slice(0, half);
-    let last = nums.slice(half, nums.length);
-    
+    let first = nums.slice(0, n);
+    let last = nums.slice(n, nums.length);
     // console.log(first, last);
     
-    for(let i=0; i<half; i++){
+    for(let i=0; i<n; i++){
         arr.push(first[i], last[i]);
     }
-    // console.log(arr);
     return arr;
 };
