e1892aeecaa7bbd9c0b8e966c80be5ce7b5670f5
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-02-08 14:10:49 +0600

    Time: 85 ms (53.18%), Space: 43.8 MB (80.83%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -13,7 +13,7 @@ var jump = function(nums) {
         for(let i=0; i<=right; i++){
             mx = Math.max(mx, i + nums[i]);
         }
-        left = right+1;
+        left = right;
         right = mx;
         jump++;
     }
