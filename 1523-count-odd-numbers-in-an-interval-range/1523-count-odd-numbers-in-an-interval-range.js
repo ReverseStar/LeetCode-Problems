d64c4ef728ca6513f4d60a85309e0f49fba5f622
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-02-13 18:15:33 +0600

    Time: 47 ms (99.68%), Space: 41.8 MB (50.76%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -6,3 +6,14 @@
 var countOdds = function(low, high) {
     return Math.floor((high+1) / 2) - Math.floor(low / 2);
 };
+
+
+/* Explanation: for the case "low = 3" and "high = 7"
+        if low = 3 and high = 7, then
+          Math.floor((high + 1) / 2) 
+        = Math.floor(8 / 2) = 4 and 
+          Math.floor(low / 2) 
+        = Math.floor(3 / 2) = 1. 
+        So, the number of odd numbers between low and high is 
+        4 - 1 = 3.
+*/
