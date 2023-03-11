63e94ebd9ddc71b96a3fdb4168fd72fc258d7981
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-03-11 20:38:55 +0600

    Time: 90 ms (10.58%), Space: 46 MB (5.64%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.js
@@ -5,46 +5,14 @@
  */
 var targetIndices = function(nums, target) {
     let sorted = nums.sort((a,b) => a-b);
+    console.log(sorted);
     let res = [];
     
-    const BS = (arr, target) => {
-        let left = 0;
-        let right = arr.length - 1;
-        
-        while (left <= right) {
-            let mid = Math.floor((left+right) / 2);
-            
-            if (arr[mid] === target) {
-                // Keep adding the matching index to the result array and shift the search window to the left
-                res.push(mid);
-                left = mid - 1;
-                
-                while (left >= 0 && arr[left] === target) {
-                    res.push(left);
-                    left--;
-                }
-                
-                // Shift the search window to the right
-                right = mid + 1;
-                
-                while (right < arr.length && arr[right] === target) {
-                    res.push(right);
-                    right++;
-                }
-                
-                // Exit the loop since all occurrences have been found
-                break;
-            }
-            
-            if (arr[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
+    for(let i=0; i<sorted.length; i++){
+        if(sorted[i] > target) break;
+        if(sorted[i] === target) res.push(i);
     }
     
-    BS(sorted, target);
-    return res.sort((a, b) => a - b);
+    return res;
 };
 
