6278e85318856dbc02e3bef197362fe1aa239ec2
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-06-25 22:00:01 +0600

    Time: 72 ms (8.70%), Space: 41.9 MB (90.53%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.js
+++ 0118-pascals-triangle/0118-pascals-triangle.js
@@ -3,32 +3,15 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-    let res = [];
+    let res = [[1]];
     
-    for(let i=0; i<numRows; i++) {
+    for(let i=1; i<numRows; i++) {
         let row = [];
         for(let j=0; j<=i; j++){
-            if(j===0 || j===i) {
-                row.push(1);
-            } else {
-                row.push(res[i-1][j] + res[i-1][j-1]);
-            }
+            row.push((res[i-1][j-1] || 0) + (res[i-1][j] || 0));
         }
         res.push(row);
     }
     return res;
 };
 
-    // let res = [];
-    // for(let i=0;i<numRows;i++){
-    //     let temp=[];
-    //     for(let j=0;j<=i;j++){
-    //         if(j==0 || j==i){
-    //             temp.push(1);
-    //         }else{
-    //             temp.push(res[i-1][j]+res[i-1][j-1]);
-    //         }
-    //     }
-    //     res.push(temp);
-    // };
-    // return res;
