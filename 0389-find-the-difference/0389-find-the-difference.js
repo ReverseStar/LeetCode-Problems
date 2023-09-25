4c93a189b2f7f73f27525149a7649d7f4be72581
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-09-25 15:14:43 +0600

    Time: 70 ms (17.47%), Space: 45.3 MB (28.31%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -4,13 +4,23 @@
  * @return {character}
  */
 var findTheDifference = function(s, t) {
-    let sum_s = 0;
-    let sum_t = 0;
+//     let sum_s = 0;
+//     let sum_t = 0;
     
-    for(let ch in s) sum_s += s.charCodeAt(ch);
-    for(let ch in t) sum_t += t.charCodeAt(ch);
+//     for(let ch in s) sum_s += s.charCodeAt(ch);
+//     for(let ch in t) sum_t += t.charCodeAt(ch);
     
-    console.log(sum_s, sum_t);
-    return String.fromCharCode(sum_t - sum_s);
+//     console.log(sum_s, sum_t);
+//     return String.fromCharCode(sum_t - sum_s);
+    let sum = 0;
+    
+    for(let ch in s){
+        sum ^= s.charCodeAt(ch);
+    }
+    for(let ch in t){
+        sum ^= t.charCodeAt(ch);
+    }
+    
+    return String.fromCharCode(sum);
 };
 
