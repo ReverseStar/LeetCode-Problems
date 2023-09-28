1c5297b0c51903e26271ca912b8c335728daf646
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-09-28 14:06:50 +0600

    Time: 73 ms (23.84%), Space: 45.1 MB (16.47%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.js
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.js
@@ -3,10 +3,18 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    let odds = nums.filter(num => num%2 !== 0);
-    // console.log(odds);
-    let evens = nums.filter(num => num%2 === 0);
-    // console.log(evens);
+//     let odds = nums.filter(num => num%2 !== 0);
+//     let evens = nums.filter(num => num%2 === 0);
     
-    return [...evens, ...odds];
+//     return [...evens, ...odds];
+    
+    let left = 0;
+    
+    for(let right=0; right<nums.length; right++){
+        if(nums[right] % 2 === 0){
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            left++;
+        }
+    }
+    return nums;
 };
