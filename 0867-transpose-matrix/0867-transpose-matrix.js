1a612b42e462754259c591bba2a7055844521f8f
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-12-10 14:23:38 +0600

    Time: 66 ms (30.48%), Space: 45.3 MB (6.15%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} matrix
+ * @return {number[][]}
+ */
+var transpose = function(matrix) {
+    let row = matrix.length;
+    let col = matrix[0].length;
+    
+    //creating matrix by filling with 0
+    let res = new Array(col).fill(0).map(() => new Array(row).fill(0));
+    // console.log(res);
+    
+    for(let r=0; r<row; r++){
+        for(let c=0; c<col; c++){
+            res[c][r] = matrix[r][c]; //placing row in col and col in row
+        }
+    }
+    
+    return res;
+};
