76884b22b7582cc90c43163274c4923ed74bfdd1
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-12-15 14:38:50 +0600

    Time: 44 ms (96.35%), Space: 44.3 MB (52.49%) - LeetHub

---
+++ 1436-destination-city/1436-destination-city.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string[][]} paths
+ * @return {string}
+ */
+var destCity = function(paths) {
+    let s = new Set();
+    
+    for(let p of paths){
+        s.add(p[0]);
+    }
+    
+    for(let p of paths){
+        if(!s.has(p[1])){
+            return p[1];
+        }
+    }
+};
