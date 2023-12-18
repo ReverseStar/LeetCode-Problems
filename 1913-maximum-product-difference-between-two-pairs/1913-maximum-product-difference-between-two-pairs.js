5b249b228234204e22c448bc448d4bcf3fbefb47
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-12-18 18:43:20 +0600

    Time: 59 ms (93.94%), Space: 45.2 MB (30.42%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -3,9 +3,27 @@
  * @return {number}
  */
 var maxProductDifference = function(nums) {
-    let asc = [...nums].sort((a,b) => a - b);
-    let desc = [...nums].sort((a,b) => b - a);
-    // console.log(asc, desc);
+    let max1 = 0;
+    let max2 = 0;
+    let min1 = Infinity;
+    let min2 = Infinity;
     
-    return Math.abs((asc[0]*asc[1]) - (desc[0]*desc[1]));
-};
+    for(let n of nums){
+        if(n > max1){
+            max2 = max1;
+            max1 = n;
+        } else {
+            max2 = Math.max(max2, n);
+        }
+        
+        if(n < min1){
+            min2 = min1;
+            min1 = n;
+        } else {
+            min2 = Math.min(min2, n);
+        }
+    }
+    // console.log(max1, max2, min1, min2);
+    return ((max1 * max2) - (min1 * min2));
+}
+    
