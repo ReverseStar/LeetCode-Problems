1ac60014e5bd6891da04e124ae5e599ee3b97a98
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-12-20 14:05:50 +0600

    Time: 87 ms (56.67%), Space: 44.8 MB (100.00%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.js
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 var buyChoco = function(prices, money) {
-    let sorted = prices.sort((a,b) => a-b);
-    let leftover = 0;
+    let min1 = Infinity;
+    let min2 = Infinity;
     
-    for(let i=0; i<sorted.length; i++){
-        for(let j=i+1; j<sorted.length; j++){
-            if(sorted[i] + sorted[j] <= money){
-                leftover = money - (sorted[i]+sorted[j]);
-                // console.log('leftover: ', leftover);
-                if(leftover => 0) return leftover;
-            }
+    for(let n of prices){
+        if(n < min1){
+            min2 = min1;
+            min1 = n;
+        } else {
+            min2 = Math.min(min2, n);
         }
     }
-    return money;
+    
+    return (money - (min1+min2)) >= 0 ? money - (min1+min2) : money;
 };
