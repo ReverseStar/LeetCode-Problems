9d857ee0a67dc1fefdb63a979667e11d6fb15060
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2023-12-23 18:52:05 +0600

    Time: 52 ms (56.12%), Space: 42 MB (74.10%) - LeetHub

---
+++ 1496-path-crossing/1496-path-crossing.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} path
+ * @return {boolean}
+ */
+var isPathCrossing = function(path) {
+    let hashset = new Set();
+    let x = 0;
+    let y = 0;
+    hashset.add(`${x},${y}`);
+    
+    for(let dir of path){
+        if(dir === 'N') y++;
+        else if(dir === 'S') y--;
+        else if(dir === 'E') x++;
+        else x--;
+        
+        let point = `${x},${y}`;
+        if(hashset.has(point)){
+            return true;
+        } else {
+            hashset.add(point);
+        }
+    }
+    return false;
+};
