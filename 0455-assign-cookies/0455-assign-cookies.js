b3e2f04a2a4fe8b202a6fbb632d8c09741d7c616
Mahmud Hasan <36154000+ReverseStar@users.noreply.github.com>
2024-01-01 18:25:32 +0600

    Time: 116 ms (9.52%), Space: 44.5 MB (78.33%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    g.sort((a,b) => a-b);
+    s.sort((a,b) => a-b);
+    
+    let i = 0;
+    let j = 0;
+    
+    while(i < g.length){
+        while(j < s.length && g[i] > s[j]){
+            j++;
+        }
+        if(j === s.length) break;
+        i++;
+        j++;
+    }
+    return i;
+};
